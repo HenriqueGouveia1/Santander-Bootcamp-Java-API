dbce7c9887c14ef66fa1037e824fd44032edc7f9
HenriqueGouveia1 <henriquegs002@gmail.com>
2023-10-04 14:30:07 -0300

    Initial commit

---
+++ src/main/java/me/HenriqueGouveia1/Santander/Bootcamp/Java/API/SantanderBootcampJavaApiApplication.java
@@ -0,0 +1,13 @@
+package me.HenriqueGouveia1.Santander.Bootcamp.Java.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderBootcampJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderBootcampJavaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/HenriqueGouveia1/Santander/Bootcamp/Java/API/SantanderBootcampJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.HenriqueGouveia1.Santander.Bootcamp.Java.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderBootcampJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
